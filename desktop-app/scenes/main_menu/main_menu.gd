b40e7d052bde68178ed12e165d1e45d255de28a3
Gabriel Mazo <88992757+Abbbbajur@users.noreply.github.com>
2025-12-03 20:03:13 -0300

    UI OrderMenu

--- desktop-app/scenes/main_menu/main_menu.gd
+++ desktop-app/scenes/main_menu/main_menu.gd
@@ -3,5 +3,11 @@ extends Control
 func _on_products_pressed():
 	get_tree().change_scene_to_packed(load("res://scenes/product_menu/product_menu.tscn") as PackedScene)
 
+func _on_orders_pressed():
+	get_tree().change_scene_to_packed(load("res://scenes/order_menu/order_menu.tscn") as PackedScene)
+
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+
